a53e5481c956f85f387d361bcbd2a6f4b28eac8c
snunit <snunit.singal@gmail.com>
2019-08-27 23:07:18 +0300

    create new project

---
+++ src/test/java/singal/GetService/GetServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package singal.GetService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GetServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
